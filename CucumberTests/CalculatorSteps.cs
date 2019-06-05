d9e071a0ea020e5b72e06f3420c8ded1b4d10d33
Dmytro Yehorov <mrdiemath@outlook.com>
2019-06-05 15:37:20 +0300

    Initial commit

---
+++ CucumberTests/CalculatorSteps.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+using Calculator;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private int result;
+        private Calculator.Calculator calculator = new Calculator.Calculator();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calculator.SecondNumber = number;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
